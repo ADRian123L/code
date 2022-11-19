ef4afc4a43f576f06dd6c555cf07771e4d4b3330
ADRian123L <adrian163@usf.edu>
2022-11-18 19:06:29 -0500

    Added content

--- CodeNotes/Classes.py
+++ CodeNotes/Classes.py
@@ -3,20 +3,20 @@
 # Base class:
 class Computer:
     
-    def __init__(self : any) -> None:
+    def __init__(self : any, memory : float, speed : float) -> None:
         """The initializer method adds attributes that electronic 
         devices have."""
-        self.cpu_speed = 0.0
-        self.memory = 0.0
+        self.__cpu_speed = speed
+        self.__memory = memory
         return None
 
 # Derived class from computer:
 class WireLess(Computer):
 
-    def __init__(self : any) -> None:
+    def __init__(self : any, memory : float, speed : float) -> None:
         """The initializer method adds attributes for wireless devices."""
-        Computer.__init__(self)
-        self.battery = 0.0
+        Computer.__init__(self, memory, speed)
+        self.__battery = 0.0
         return None
 
 # Derived class from wireless:
@@ -28,12 +28,12 @@ class Phone(WireLess):
 # Derived class from wireless:
 class Tablet(WireLess):
 
-    def __init__(self : any) -> None:
+    def __init__(self : any, speed : float, memory : float) -> None:
         """The initializer method retrieves the attributes of its base
         classes and adds two more attributes."""
-        WireLess.__init__(self)
-        self.brand = ""
-        self.screen_size = 0.0
+        WireLess.__init__(self, speed, memory)
+        self._brand = ""
+        self._screen_size = 0.0
         return None
 
     def __str__(self : any) -> str:
@@ -42,10 +42,9 @@ and screen size of {self.screen_size}."
         return format
 
 if __name__ == "__main__":
-    ipad = Tablet()
+    ipad = Tablet(100.00, 300.00)
     ipad.brand = "Ipad"
     ipad.cpu_speed = 100
-    ipad.memory = 200
     ipad.battery = 300
     ipad.screen_size = 400
     print(ipad)

322731ed3167b7614cb0f9ccde7625283d97fc35
ADRian123L <adrian163@usf.edu>
2022-11-29 11:29:02 -0500

    Created a widget object.

--- CodeNotes/TKinter.py
+++ CodeNotes/TKinter.py
@@ -5,3 +5,20 @@
 """
 
 import tkinter
+
+class GUIwindow:
+
+    def __init__(self : any) -> None:
+        """The method initializers the object with an attribute
+        that adds a widget and other features."""
+        # Creates a widget:
+        self.window = tkinter.Tk()
+        # Adds a title to the widget:
+        self.window.title("Index")
+        # The loop:
+        tkinter.mainloop()
+        return None
+        
+if __name__ == "__main__":
+    # Creates an object with attributes of a widget:
+    window = GUIwindow()

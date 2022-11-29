c62ad2f322d2aaf8fa9329f7182dae36d6ffbefc
ADRian123L <adrian163@usf.edu>
2022-11-29 12:17:10 -0500

    Added two widget objects.

--- CodeNotes/TKinter.py
+++ CodeNotes/TKinter.py
@@ -15,10 +15,41 @@ class GUIwindow:
         self.window = tkinter.Tk()
         # Adds a title to the widget:
         self.window.title("Index")
+        # Creates a button:
+        self.button = tkinter.Button(master=self.window, text="Button")
         # The loop:
         tkinter.mainloop()
         return None
-        
+
+    def quite(self : any) -> None:
+        """The method breaks the widget loop."""
+        tkinter.Message.quit()
+        return None
+
+class Widget:
+    
+    def __init__(self : any) -> None:
+        """The method initializers the object with an attribute
+        that adds a widget and other features."""
+        # Creates a widget:
+        self.window = tkinter.Tk()
+        self.window.title("Password")
+        # Creates an input box:
+        self.frame = tkinter.Frame()
+        self.frame2nd = tkinter.Frame()
+        self.prompt = tkinter.Label(self.frame)
+        # Starts the loop:
+        tkinter.mainloop()
+        return None
+
+def main(first=False, second=False) -> None:
+    """The function drives the program."""
+    # Creates the objects:
+    if (first == True):
+        window = GUIwindow()
+    if (second == True):
+        window2 = Widget()
+    return None
+
 if __name__ == "__main__":
-    # Creates an object with attributes of a widget:
-    window = GUIwindow()
+    main(second=True)
